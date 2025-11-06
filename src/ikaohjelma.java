6f6cfbda9ca14e44910b038628184f2b30184696
szymon.szuwarowski <szuwarowskiszymon2@gmail.com>
2025-11-06 11:28:48 +0200

    ikaohjelman luonti

---
+++ src/ikaohjelma.java
@@ -0,0 +1,25 @@
+public class ikaohjelma {
+    public static void main(String[] args) {
+        int ika = 16;
+
+    if (ika > 0 && ika < 18) {
+
+System.out.println("Olet alaikäinen");
+
+
+
+} else if (ika >= 65 && ika < 120) {
+
+System.out.println("Olet eläkeläinen");
+
+
+} else {
+
+System.out.println("Olet aikuinen");
+
+
+
+} }
+}
+
+
